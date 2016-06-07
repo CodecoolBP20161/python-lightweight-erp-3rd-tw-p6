9eed8a33d3b018ba0cdb63ecbfdc05751719390b
handriss <andrashinkel@gmail.com>
2016-06-07 15:25:49 +0200

    Table print even more pretty

--- ui.py
+++ ui.py
@@ -1,4 +1,4 @@
-
+import copy
 
 # An example output:
 # /-----------------------------------\
@@ -10,18 +10,39 @@
 # \-----------------------------------/
 def print_table(table, title_list):
     """ Pretty prints a table. """
+    table = copy.deepcopy(table)
+
     table.insert(0, title_list)
 
-    # s = [[str("| " + e) for e in row] for row in table]
-    s = [[(e) for e in row] for row in table]
+    s = [[str("| " + e) for e in row] for row in table]
     lens = [max(map(len, col)) for col in zip(*s)]
-    fmt = '\t'.join('{{:<{}}}'.format(x) for x in lens)
+    fmt = ' '.join('{{:<{}}}'.format(x) for x in lens)
     table = [fmt.format(*row) for row in s]
 
     length = sum(lens)
+
     for i in range(len(table)):
-        table.insert(2*i + 1, "-"*(length + 8*(len(title_list)-1)))
-    table.insert(0, "-"*(length + 8*(len(title_list)-1)))
+        table[i*2] += " |"
+        table.insert(2*i + 1, "-"*(length + 2*(len(title_list)-1)))
+
+    table.insert(0, "-"*(length + 2*(len(title_list)-1)))
+
+    corner = list(table[0])
+    corner[0] = "/"
+    corner[-1] = "\\"
+    table[0] = "".join(corner)
+    corner = list(table[-1])
+    corner[0] = "\\"
+    corner[-1] = "/"
+    table[-1] = "".join(corner)
+
+    row_list = list(table[1])
+    for i in range(len(table[0])):
+        if row_list[i] == "|":
+            for k in range(1, len(table)-1):
+                row_list2 = list(table[k])
+                row_list2[i] = "|"
+                table[k] = "".join(row_list2)
 
     print ('\n'.join(table))
 
