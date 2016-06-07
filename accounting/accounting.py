4baf67113356d6912c821961ea9aa8d42e8a24a1
dsztanko <szt.dori@gmail.com>
2016-06-07 15:16:46 +0200

    accounting.py remove and update function added

--- accounting/accounting.py
+++ accounting/accounting.py
@@ -31,54 +31,76 @@ def start():
         if user_input == "0":
             break
         elif user_input == "1":
-            show_table("items.csv")
+            show_table("accounting/accounting/items.csv")
         elif user_input == "2":
-            add("items.csv")
+            add("accounting/items.csv")
         elif user_input == "3":
-            remove("items.csv", id_)
+            list_titles = ["Id_: "]
+            record = ui.get_inputs(list_titles, title="")
+            remove("accounting/items.csv", record)
         elif user_input == "4":
-            update("items.csv", id_)
+            list_titles = ["Id_: "]
+            record = ui.get_inputs(list_titles, title="")
+            update("accounting/items.csv", record)
         else:
             ui.print_error_message("Invalid input!")
 
 
 # print the default table of records from the file
 def show_table(table):
-    """ Shows the items.csv database """
-    table = data_manager.get_table_from_file("items.csv")
+    """ Shows the accounting/items.csv database """
+    table = data_manager.get_table_from_file("accounting/items.csv")
     title_list = ["id", "month", "day", "year", "type", "amount"]
-    for rows in table:
-        print(rows)
-    # return ui.print_table(table, title_list)
+    return ui.print_table(table, title_list)
 
 
 # Ask a new record as an input from the user than add it to @table, than return @table
 def add(table):
-        """ Adds a new row to the database """
-        id_q_input = common.generate_random("items.csv")
-        list_titles = ["Month: ", "Day: ", "Year: ", "Type: ", "Amount: "]
-        record = ui.get_inputs(list_titles, title="")
-        record.insert(0, id_q_input)
-        table = data_manager.get_table_from_file("items.csv")
-        table = table + [record]
-        table = data_manager.write_table_to_file("items.csv", table)
-        # return ui.print_table(table, title_list)
+    """ Adds a new row to the database """
+    id_input = common.generate_random("accounting/items.csv")
+    list_titles = ["Month: ", "Day: ", "Year: ", "Type: ", "Amount: "]
+    record = ui.get_inputs(list_titles, title="")
+    record.insert(0, id_input)
+    table = data_manager.get_table_from_file("accounting/items.csv")
+    table = table + [record]
+    table = data_manager.write_table_to_file("accounting/items.csv", table)
+    return table
 
 
 # Remove the record having the id @id_ from the @list, than return @table
 def remove(table, id_):
-
-    # your code
-
+    """ Removes a certain row from the database identified by id_ """
+    table = data_manager.get_table_from_file("accounting/items.csv")
+    existing_id = [i[0] for i in table]
+    id_ = ''.join(id_)
+    if id_ in existing_id:
+        id_index = existing_id.index(id_)
+        table.remove(table[id_index])
+        table = data_manager.write_table_to_file("accounting/items.csv", table)
+    else:
+        ui.print_error_message("Invalid input!")
     return table
 
 
 # Update the record in @table having the id @id_ by asking the new data from the user,
 # than return the @table
 def update(table, id_):
-
-    # your code
-
+    """ Removes a certain row from the database identified by id_ """
+    table = data_manager.get_table_from_file("accounting/items.csv")
+    existing_id = [i[0] for i in table]
+    id_ = ''.join(id_)
+    if id_ in existing_id:
+        id_index = existing_id.index(id_)
+        list_titles = ["Month: ", "Day: ", "Year: ", "Type: ", "Amount: "]
+        updated_inputs = ui.get_inputs(list_titles, title="")
+        table[id_index][1] = updated_inputs[0]
+        table[id_index][2] = updated_inputs[1]
+        table[id_index][3] = updated_inputs[2]
+        table[id_index][4] = updated_inputs[3]
+        table[id_index][5] = updated_inputs[4]
+        table = data_manager.write_table_to_file("accounting/items.csv", table)
+    else:
+        ui.print_error_message("Invalid input!")
     return table
 
 
